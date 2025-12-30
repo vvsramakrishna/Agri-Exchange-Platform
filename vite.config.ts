fb58405d0432e055894b9fce905be446853fbbc9
vvsramakrishna <vvsramakrishna98@gmail.com>
2025-12-30 23:41:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      base: '/',
       },
       resolve: {
         alias: {
