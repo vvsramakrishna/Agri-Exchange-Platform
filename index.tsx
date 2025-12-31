fea066bd701c4ba28f158d9a17c0a924c8472e00
vvsramakrishna <vvsramakrishna98@gmail.com>
2025-12-31 12:14:00 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+console.log('ENV:', import.meta.env.VITE_GEMINI_API_KEY);
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
